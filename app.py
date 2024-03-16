cd973ad94083e151740850c00b9a3920f4774534
Juan Mendez <juanvmendez@csus.edu>
2024-03-16 14:16:57 -0700

    Merged code for login in app.py and added required html

--- app.py
+++ app.py
@@ -1,11 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, session
 from database import db_blueprint, get_all_parts, checkout_part, checkin_part
 from flask import request, redirect, url_for
 
 app = Flask(__name__)
 app.register_blueprint(db_blueprint)
+app.secret_key = 'your_secret_key'
 
-@app.route('/')
+#Temporary User Dictionary
+USER_CREDENTIALS = {
+    'admin': 'password',
+    'user1': '123456'
+}
+
+#Authenticates User Creds
+def authenticate_user(username, password):
+    if username in USER_CREDENTIALS and USER_CREDENTIALS[username] == password:
+        return True
+    return False
+
+#The Routes
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if authenticate_user(username, password):
+            session['username'] = username
+            return redirect('/dashboard')
+        else:
+            return render_template('login.html', message = 'Invalid username or password')
+    return render_template('login.html')
+
+@app.route('/dashboard')
 def pc_parts():
     parts = get_all_parts()
     return render_template('search_results.html', parts=parts)
@@ -31,6 +57,11 @@ def checkin():
     checked_out = request.form['checked_out']
     checkin_part(serial_number, part_type, brand, shelf_location, checked_out)
     return redirect(url_for('pc_parts'))
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect('/login')
 
 
 if __name__ == '__main__':
