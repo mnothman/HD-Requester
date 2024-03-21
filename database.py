e84ab0fd2f7f2a664e6ef97af372ab8a2c59d056
mnothman <mohammadothman113@gmail.com>
2024-03-21 15:07:42 -0700

    Small changes

--- database.py
+++ database.py
@@ -17,6 +17,8 @@ def close_db(error):
     if db is not None:
         db.close()
 
+    current_app.logger.info("Database is connected")
+
 def get_all_parts(search_type=None):
     db = get_db()
     query = 'SELECT * FROM parts'
@@ -27,7 +29,7 @@ def get_all_parts(search_type=None):
     parts = db.execute(query, args).fetchall()
     return parts
 
-def checkout_part(serial_number):
+def checkout_part(serial_number): #need to change these buttons to not submit
     db = get_db()
     db.execute('UPDATE parts SET checked_out = 1 WHERE serial_number = ?', (serial_number,))
     db.commit()
@@ -38,3 +40,12 @@ def checkin_part(serial_number, part_type, brand, shelf_location, checked_out): 
     db.execute('INSERT INTO parts (serial_number, part_type, brand, shelf_location, checked_out) VALUES (?, ?, ?, ?, 0)',  #comes as 0 for checked_out
                (serial_number, part_type, brand, shelf_location))
     db.commit()
+
+
+# we dont need login for the inventory page to check in and out, we only need a login for the admin page. open new window for clicking the admin page which promps for a login. 
+#only use ajax for login page so it doesnt refresh the page when you click go or submit.
+# bootstrap to display inventory/admin page
+#default is IN, and then you can switch to out
+    
+
+    #the go button is like the submit, want to do a lot of actions at once, so we use the go button to do all the actions at once.
