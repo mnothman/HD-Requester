86c42f1e05edcf537076d14e6d36c1713351d61a
delia13nh <91099025+delia13nh@users.noreply.github.com>
2024-03-21 18:19:11 -0700

    Add files via upload

---
+++ modal.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+document.getElementById('open_button').addEventListener('click', function(){
+			document.querySelector('.background-modal').style.display='flex'});
+
+
+document.querySelector('.close').addEventListener('click'),function(){
+	document.querySelector('background-modal').style.display='none' };
+														
