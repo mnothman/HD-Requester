ead9cd1ed289484d31005374568d39d14a24d720
delia13nh <91099025+delia13nh@users.noreply.github.com>
2024-03-31 19:30:41 -0700

    Delete modal.js
    The code will be included in the file modal.html for practical purposes

--- modal.js
+++
@@ -1,8 +0,0 @@
-// JavaScript Document
-document.getElementById('open_button').addEventListener('click', function(){
-			document.querySelector('.background-modal').style.display='flex'});
-
-
-document.querySelector('.close').addEventListener('click'),function(){
-	document.querySelector('background-modal').style.display='none' };
-														
