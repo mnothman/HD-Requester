bb1e03de3a596719871c4610dc20c695c5868063
Richard Clinger <rclinger@csus.edu>
2024-04-14 22:19:47 -0700

    only show parts with status 'in'

--- database.py
+++ database.py
@@ -21,10 +21,10 @@ def close_db(error):
 
 def get_all_parts(search_type=None):
     db = get_db()
-    query = 'SELECT Part.* FROM Part JOIN Part_log ON Part.Part_sn = Part_log.Part_sn'
+    query = 'SELECT Part.* FROM Part JOIN Part_log ON Part.Part_sn = Part_log.Part_sn WHERE Part_log.Part_status IS \'in\''
     args = ()
     if search_type:
-        query += ' WHERE Part_log.Part_status IS 'in' AND Type LIKE ?'
+        query += ' AND Type LIKE ?'
         args = ('%' + search_type + '%',)
     parts = db.execute(query, args).fetchall()
     return parts
