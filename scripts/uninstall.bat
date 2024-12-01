52a06629db24d14b19cc3254ee3a03394cb9e3ac
Juan Mendez <juanvmendez@csus.edu>
2024-12-01 14:09:58 -0800

    Fixed install.bat and uninstall.bat

--- scripts/uninstall.bat
+++ scripts/uninstall.bat
@@ -1,9 +1,8 @@
-<<<<<<< HEAD
 @echo off
 setlocal
 
 :: Set the desired Python version
-set "desired_version=3.11.7"
+set "desired_version=3.13.0"
 
 :: Check if a Python is installed and its version
 for /f "delims=" %%i in ('python --version 2^>nul') do (
@@ -29,36 +28,4 @@ if "%installed_version%"=="" (
 echo.
 echo uninstall.bat has completed.
 pause
-=======
-@echo off
-setlocal
-
-:: Set the desired Python version
-set "desired_version=3.11.7"
-
-:: Check if a Python is installed and its version
-for /f "delims=" %%i in ('python --version 2^>nul') do (
-    set "installed_version=%%i"
-)
-
-:: Compare installed version with desired version
-if "%installed_version%"=="" (
-    echo Python is not installed. Uninstall was successful.
-    goto :end
-) else (
-    echo Manual uninstall process beginning. Opening control pannel.
-    start appwiz.cpl
-    echo.
-    echo Manual Insstructions
-    echo 1.   Click on the installed python.
-    echo 2.   When requested to make changes click yes.
-    echo.
-    echo Click on uninstall.bat again to confirm python has been uninstalled.
-)
-
-:end
-echo.
-echo uninstall.bat has completed.
-pause
->>>>>>> ecdf7b17a29d555c8e15f3032be865fb2dc50539
 endlocal
